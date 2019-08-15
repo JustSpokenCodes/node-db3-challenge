a160f60cde33e87d6a95c48dbc3031f30ec251a5
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-08-15 11:44:11 -0700

    scheme-model done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,3 +9,36 @@ module.exports = {
     remove
 }
 
+function find() {
+    return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes').where({id}).first();
+}
+
+function findSteps(scheme_id) {
+    return db('steps as st')
+        .join('schemes as s', 's.id', 'st.scheme_id')
+        .select('st.id', 's.schemes', 's.intructions')
+        .where({scheme_id});
+}
+
+async function add(schemes) {
+    const [id] = await db('schemes').insert(schemes);
+    return findById(id);
+}
+
+async function update(changes, id) {
+    await db('schemes')
+        .where({id})
+        .update(changes);
+
+    return findById(id);
+}
+
+function remove(id) {
+    return db('users')
+        .where({id})
+        .del();
+}
